62eb3e7a555dece5c319f8281c600cdda9cc962a
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 15:31:04 -0700

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -52,7 +52,6 @@ global.etiqueta = 'Legna'
 
 global.moneda = 'YamiCoins'
 global.catalogo = fs.readFileSync(path.join(process.cwd(), 'src', 'menu.jpg'));
-global.avatar = 'https://tinyurl.com/25ru293g'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
