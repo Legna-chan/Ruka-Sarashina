b3ce9e70330e5dd69e6b3af8654e49c34182f2f2
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 12:56:56 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -17,20 +17,20 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `「✿」Kafuu - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Usa *#ayuda* para ver mi menu.
-> 🜸 https://github.com/Legna-chan`;
+    let welcome = `「🦋」Yami - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Usa *#ayuda* para ver mis comandos.
+> 🅜 ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥣᥱgᥒᥲ`;
     await conn.sendMini(m.chat, packname, textbot, welcome, img, img, redes, fkontak);
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = ` 「✿」Kafuu - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver menu.
-> 🜸 https://github.com/Legna-chan`;
+    let bye = ` 「🦋」Yami - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver mis comandos.
+> 🅜 ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥣᥱgᥒᥲ`;
     await conn.sendMini(m.chat, packname, textbot, bye, img, img, redes, fkontak);
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `「✿」Kafuu - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3」\n\n> ✐ Usa *#ayuda* para ver mi menu. 
-> 🜸 https://github.com/Legna-chan`;
+    let kick = `「🦋」Yami - MD \n「 Adios 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3」\n\n> ✐ Usa *#ayuda* para ver mis comandos. 
+> 🅜 ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥣᥱgᥒᥲ`;
     await conn.sendMini(m.chat, packname, textbot, kick, img, img, redes, fkontak);
     await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] });
   }
