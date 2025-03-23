ed9452e86ce4345993b4b13cdace916e66b1f861
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 14:29:52 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -509,7 +509,7 @@ let handler = async (m, { conn, args }) => {
           externalAdReply: {
               title: botname,
               body: textbot,
-              thumbnail: banner,
+              thumbnail: catalogo,
               mediaType: 1,
               showAdAttribution: true,
               renderLargerThumbnail: true,
