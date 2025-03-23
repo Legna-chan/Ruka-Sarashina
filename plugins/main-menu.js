c601932d6ae707c302fbc6a47e71297d27c448b6
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 14:46:06 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -11,14 +11,14 @@ let handler = async (m, { conn, args }) => {
  
     let txt = `
 *!𝙷𝚘𝚕𝚊! 𝚎𝚜𝚝𝚎 𝚎𝚜 𝚎𝚕 𝚖𝚎𝚗𝚞 𝚍𝚎 ${botname}*
-╭┈ ↷
-> ✐ Cliente ➩ @${userId.split('@')[0]}
-> ❀ Modo ➩ Publica
-> ✦ Bot ➩ ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Sub-bot 🆂')}
-> ⴵ Activa ➩ ${uptime}
-> ✰ Usuarios ➩ ${totalreg}
-> ❏ version ➩ 1.0.0
-╰─────────────────
+╭──❀•°❀°•❀──╮  
+┃ ✦ Cliente ➩ @${userId.split('@')[0]}  
+┃ ❀ Modo ➩ Publica  
+┃ ✦ Bot ➩ ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Sub-bot 🆂')}  
+┃ ⴵ Activa ➩ ${uptime}  
+┃ ✰ Usuarios ➩ ${totalreg}  
+┃ ❏ Versión ➩ 1.0.0  
+╰──❀•°❀°•❀──╯
 
 • :･ﾟ⊹˚• \`『 Info-Bot 』\` •˚⊹:･ﾟ•
 
