688d105b62d315c6f51e03b20638bf2f4ca4f41a
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 12:50:30 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,9 +4,6 @@ import fetch from 'node-fetch'
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
   let img = fs.readFileSync(path.join(process.cwd(), 'src', 'welcome.jpg'));
-} catch (error) {
-  console.error('No se pudo cargar la imagen localmente:', error);
-}
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
