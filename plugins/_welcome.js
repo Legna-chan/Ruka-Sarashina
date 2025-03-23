fc288b0425cd36f4ae7b14867892145e1c741ff3
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-23 12:19:22 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { WAMessageStubType } from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
 
@@ -9,20 +11,20 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   if (chat.welcome) {
     let who = m.messageStubParameters[0]?.replace('@', '') + '@s.whatsapp.net';
-    
+
     if (m.messageStubType === WAMessageStubType.ADD) {
-      let welcome = `「✿」Yami - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Usa *#ayuda* para ver mi menú.`;
+      let welcome = `「✿」Yami - MD \n「 Bienvenido :3 」\n「 @${m.messageStubParameters[0].split('@')[0]} 」\n「 Bienvenido/a 」\n「 ${groupMetadata.subject} 」\n\n> ✐ Usa *#ayuda* para ver mi menú.`;
       await conn.sendMini(m.chat, packname, textbot, welcome, img, img, redes, fkontak);
     }
 
     if (m.messageStubType === WAMessageStubType.REMOVE) {
-      let bye = `「✿」Kafuu - MD \n「 Adiós 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver el menú.`;
+      let bye = `「✿」Kafuu - MD \n「 Adiós 」\n「 @${m.messageStubParameters[0].split('@')[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver el menú.`;
       await conn.sendMini(m.chat, packname, textbot, bye, img, img, redes, fkontak);
       await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] });
     }
 
     if (m.messageStubType === WAMessageStubType.KICK) {
-      let kick = `「✿」Kafuu - MD \n「 Adiós 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver mi menú.`;
+      let kick = `「✿」Kafuu - MD \n「 Adiós 」\n「 @${m.messageStubParameters[0].split('@')[0]} 」\n「 Se fue 」\n「 Vuelve pronto :3 」\n\n> ✐ Usa *#ayuda* para ver mi menú.`;
       await conn.sendMini(m.chat, packname, textbot, kick, img, img, redes, fkontak);
       await conn.sendMessage(m.chat, { image: img, caption: kick, mentions: [who] });
     }
