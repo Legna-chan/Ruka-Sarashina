a71f684c12dff09b29611cd1f674c821dc26c9d3
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-24 17:06:35 -0700

    Actualizar grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,24 +1,32 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
-let isClose = { // Switch Case Like :v
-'open': 'not_announcement',
-'close': 'announcement',
-'abierto': 'not_announcement',
-'cerrado': 'announcement',
-'abrir': 'not_announcement',
-'cerrar': 'announcement',
-}[(args[0] || '')]
-if (isClose === undefined)
-return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
-await conn.groupSettingUpdate(m.chat, isClose)
+let handler = async (m, { conn, args, command }) => {
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icons)
+  
+  let isClose = { // Mapa de opciones
+    'open': 'not_announcement',
+    'close': 'announcement',
+    'abierto': 'not_announcement',
+    'cerrado': 'announcement',
+    'abrir': 'not_announcement',
+    'cerrar': 'announcement',
+  }[(args[0] || '')]
+  
+  // Si no se encuentra una opción válida, se responde con un mensaje explicativo
+  if (isClose === undefined) 
+    return conn.reply(m.chat, `🍬 *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
 
-if (isClose === 'not_announcement'){
-m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
+  // Cambia la configuración del grupo
+  await conn.groupSettingUpdate(m.chat, isClose)
+
+  // Mensaje según la configuración de grupo
+  if (isClose === 'not_announcement') {
+    m.reply(`🍬 *Ya pueden escribir en este grupo.*`)
+  }
+
+  if (isClose === 'announcement') {
+    m.reply(`🍭 *Solos los admins pueden escribir en este grupo.*`)
+  }
 }
 
-if (isClose === 'announcement'){
-m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
-}}
 handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['grupo']
 handler.command = ['group', 'grupo']
