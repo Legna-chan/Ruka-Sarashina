005990999e1c4b2f1dd97410d97b89616403fb17
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-03-28 09:41:58 -0700

    Actualizar tools-clima.js

--- plugins/tools-clima.js
+++ plugins/tools-clima.js
@@ -58,6 +58,6 @@ let handler = async (m, { conn, args }) => {
 };
 
 handler.help = ['clima *<ciudad/país>*'];
-handler.tags = ['herramientas'];
+handler.tags = ['transformador'];
 handler.command = ['clima', 'tiempo']
 export default handler;
